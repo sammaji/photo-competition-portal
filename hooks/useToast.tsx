3c088d133e7aaa8d575443161e6c767adc67dfe3
Samya brata Maji <samyabratamaji334@gmail.com>
2023-04-26 01:18:16 +0530

    add: failure toasts

--- hooks/useToast.tsx
+++ hooks/useToast.tsx
@@ -1,5 +1,6 @@
 import { BsCheck } from "react-icons/bs";
 import { notifications } from "@mantine/notifications";
+import { RxCross2 } from "react-icons/rx";
 
 const useToasts = () => {
     const successToast = (message: string, title: string = "") => {
@@ -11,7 +12,16 @@ const useToasts = () => {
         });
     };
 
-    return { successToast };
+    const failureToast = (message: string, title: string = "") => {
+        notifications.show({
+            title,
+            message,
+            color: "red",
+            icon: <RxCross2 />,
+        });
+    };
+
+    return { failureToast, successToast };
 };
 
 export default useToasts;
